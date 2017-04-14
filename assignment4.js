8517a3abf0a279b106b7c291bd00f06d30294924
ard71 <ard71@pitt.edu>
2017-04-14 14:25:13 -0400

    finished

--- assignment4.js
+++ assignment4.js
@@ -11,7 +11,29 @@
 // $.html(), $.text(), etc.
 // keyup events could be helpful to get value of field as the user types
 
-(function() {
-  // Magic!
-  console.log('Keepin\'n it clean with an external script!');
-})();
+(function(){
+	$('.flexsearch-input').keyup(function(e){
+     		var input = $('.flexsearch-input').val();
+     		input = input.toLowerCase();
+     		$('.autocomplete').html("");
+     		getResults(string);
+     	});
+ 
+     function getResults(input) {
+     	googleLink = "https://www.google.com/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=";
+     	$.getJSON("http://www.mattbowytz.com/simple_api.json?data=all", function(data){
+ 		 	for (var i in data){
+   				for (var j in data[i]){
+   					var results = data[i][j];
+   					for( var k in results){
+   						var curr = results[k];
+   						curr = curr.toLowerCase();
+   						if(curr.startsWith(input) && input.length > 0 && curr.length > 1) {
+                 			$('.autocomplete').append('<a target="_blank" href=' + googleLink + curr.replace(' ', '%20') + '>' + curr + '</a><br>');
+ 		 				}
+ 		 			}
+ 		 		}
+ 			}		
+ 		});
+ 	}
+ });
